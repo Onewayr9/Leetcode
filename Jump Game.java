c70f343bf0809d8f8a45e852c409a78adde2255d
Onewayr9 <Onewayr99@gmail.com>
2015-11-19 23:35:40 -0800

    11/19

--- Jump Game.java
+++ Jump Game.java
@@ -1,16 +1,14 @@
 public class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length==0) return true;
-        if(nums[0]==0&&nums.length>1) return false;
-        int base = 0;
-        while(base+nums[base]<nums.length-1){
-            for(int i=1;i<=nums[base];i++){
-                if(nums[base+i]+base+i>base+nums[base]){
-                    base+=i;
-                    break;
-                }
-                if(i==nums[base]&&nums[base+i]==0) return false;
+        if(nums.length<=1){
+            return true;
+        }
+        int max = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i>max){
+                return false;
             }
+            max = Math.max(max,i+nums[i]);
         }
         return true;
     }
